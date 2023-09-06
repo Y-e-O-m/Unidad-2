4b814c442d055c6a2c212a06e034956045ab4ff1
Y-e-O-m <143623433+Y-e-O-m@users.noreply.github.com>
2023-09-06 08:43:29 -0600

    Add files via upload

---
+++ Proyecto.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tareas;
+        String lista = "listaTareas";
+        String tarea1 = "Barrer";
+        String tarea2 = "Tender cama";
+        String tarea3 = "Hacer trabajos";
+        String tarea4 = "Estudiar";
+        String tarea5 = "Lavar trastes";
+        
+        
+        String cadenaTexto = " cadena de texto!!! %% $$$";
+        System.out.print("Mi tarea es: " + tarea1);
+        System.out.print("Mi tarea es: " + tarea2);
+        System.out.print("Mi tarea es: " + tarea3);
+        System.out.print("Mi tarea es: " + tarea4);
+        System.out.print("Mi tarea es: " + tarea5);
+
+
+
+    }
+    
+}
