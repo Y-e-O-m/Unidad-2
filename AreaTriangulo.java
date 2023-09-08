4083c948ee3baa91795e2967c0d1a344bcf03b6c
Y-e-O-m <143623433+Y-e-O-m@users.noreply.github.com>
2023-09-08 08:46:55 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int numeroA = 10;
+        int numeroB = 4;
+        
+        int resultado = numeroB * numeroA/2;
+        
+        System.out.print(resultado);
+        
+    }
+    
+}
